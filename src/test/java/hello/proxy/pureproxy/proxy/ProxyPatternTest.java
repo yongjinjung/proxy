32621738443f093e7066b78038496328f282d3c3
YongJin Jung <dydwls1641@gmail.com>
2022-04-12 21:35:33 +0900

    Changes

--- src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
+++ src/test/java/hello/proxy/pureproxy/proxy/ProxyPatternTest.java
@@ -1,2 +1,29 @@
-package hello.proxy.pureproxy.proxy;public class ProxyPatternTest {
+package hello.proxy.pureproxy.proxy;
+
+import hello.proxy.pureproxy.proxy.code.CacheProxy;
+import hello.proxy.pureproxy.proxy.code.ProxyPatternClient;
+import hello.proxy.pureproxy.proxy.code.RealSubject;
+import hello.proxy.pureproxy.proxy.code.Subject;
+import org.junit.jupiter.api.Test;
+
+public class ProxyPatternTest {
+
+    @Test
+    void noProxyTest(){
+        RealSubject realSubject = new RealSubject();//자동 변수 선언 Ctrl + Alt + v
+        ProxyPatternClient client = new ProxyPatternClient(realSubject);
+        client.execute();
+    }
+
+    @Test
+    void cacheProxyTest(){
+        RealSubject realSubject = new RealSubject();
+        CacheProxy cacheProxy = new CacheProxy(realSubject);
+
+        ProxyPatternClient client = new ProxyPatternClient(cacheProxy);
+
+        client.execute();
+        client.execute();
+        client.execute();
+    }
 }
